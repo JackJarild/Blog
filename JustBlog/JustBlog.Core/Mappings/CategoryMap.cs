db8f381821e049725865756f0ecac46c490f90dd
Jack <jaaackk@hotmail.se>
2015-05-06 18:59:55 +0200

    First Upload

---
+++ JustBlog/JustBlog.Core/Mappings/CategoryMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JustBlog.Core.Objects;
+using FluentNHibernate.Mapping;
+
+namespace JustBlog.Core.Mappings
+{
+    class CategoryMap : ClassMap<Category>
+    {
+        public CategoryMap()
+        {
+            Id(x => x.Id);
+
+            Map(x => x.Name)
+                .Length()
+        }
+    }
+}
